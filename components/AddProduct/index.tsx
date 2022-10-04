6fdabea60bb93fa7d0c4c6b2aeaab76d5cb1037a
hyeonah <hyeonah.hello@gmail.com>
2022-10-04 17:46:58 +0900

    #4 feat: Add UI and Render Info in AddProduct component

--- components/AddProduct/index.tsx
+++ components/AddProduct/index.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { Container, Box } from '@mui/material'
+import Info from './Info'
+import style from './AddProduct.module.scss'
 
 const AddProduct = () => {
-  return <div>AddProduct</div>
+  return (
+    <Box sx={{ padding: '2rem 5rem' }}>
+      <h1 className={style['page-title']}>상품추가</h1>
+      <Container maxWidth='md'>
+        <Info />
+      </Container>
+    </Box>
+  )
 }
 
 export default AddProduct
