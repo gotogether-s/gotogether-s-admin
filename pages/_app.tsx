352ab3f6b0430b217e8821103ddd1e1928185196
hyeonah <hyeonah.hello@gmail.com>
2022-10-04 19:20:21 +0900

    #4 conf: Create addProduct slice and Provide redux store to React

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material/styles'
 import theme from '@styles/theme'
+import { Provider } from 'react-redux'
+import store from 'store'
 import Layout from '@components/Layout'
 import '@styles/reset.css'
 import '@styles/muiOverride.scss'
@@ -9,9 +11,11 @@ import '@styles/globals.scss'
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
     </ThemeProvider>
   )
 }
