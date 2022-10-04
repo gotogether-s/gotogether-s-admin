72f9fa2157802e2fbfabb675400b529292519dc2
hyeonah <hyeonah.hello@gmail.com>
2022-10-04 20:19:07 +0900

    #4 feat: Update state in AddProductSlice when input value changes

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,8 +1,18 @@
 import { TextField } from '@mui/material'
+import { useDispatch } from 'react-redux'
+import { update } from '@store/addProductSlice'
 import style from './Input.module.scss'
 
 const Input = props => {
   const { label, name, placeholder } = props
+
+  const dispatch = useDispatch()
+
+  const updateAddProductState = e => {
+    const { name, value } = e.target
+    dispatch(update({ name, value }))
+  }
+
   return (
     <div className={style['input-wrapper']}>
       <div className={style['label']}>{label}</div>
@@ -11,6 +21,7 @@ const Input = props => {
         size='small'
         name={name}
         placeholder={placeholder}
+        onChange={updateAddProductState}
       />
     </div>
   )
--- store/addProductSlice.ts
+++ store/addProductSlice.ts
@@ -1,11 +1,43 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = {}
+const initialState = {
+  thumbnail: '',
+  productName: '',
+  basicPrice: '',
+  ages: '',
+  genderGroup: '',
+  companion: '',
+  religion: '',
+  theme: '',
+  continent: '',
+  country: '',
+  region: '',
+  points: '',
+  airport: '',
+  info: '',
+  optionDtoList: [
+    {
+      name: '',
+      value: '',
+      additional: 0,
+    },
+  ],
+}
 
 const addProductSlice = createSlice({
   name: 'addProductSlice',
   initialState: initialState,
-  reducers: {},
+  reducers: {
+    update: (state, action) => {
+      const { name, value } = action.payload
+      const initialStateKey = Object.keys(state).find(
+        stateKey => stateKey === name
+      )
+      state[initialStateKey] = value
+    },
+  },
 })
 
 export default addProductSlice
+
+export const { update } = addProductSlice.actions
