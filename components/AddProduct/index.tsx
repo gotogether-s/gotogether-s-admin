ead850d27365e671054b948161abfc5d78be47be
hyeonah <hyeonah.hello@gmail.com>
2022-10-05 19:44:54 +0900

    #4 feat: Add section and Create new title and button UI

--- components/AddProduct/index.tsx
+++ components/AddProduct/index.tsx
@@ -1,3 +1,4 @@
+import AddIcon from '@mui/icons-material/Add'
 import { Container, Box } from '@mui/material'
 import Info from './Info'
 import style from './AddProduct.module.scss'
@@ -7,8 +8,19 @@ const AddProduct = () => {
     <Box sx={{ padding: '2rem 5rem' }}>
       <h1 className={style['page-title']}>상품추가</h1>
       <Container maxWidth='md'>
-        <h2 className={style['section-title']}>여행정보 (상단)</h2>
-        <Info />
+        <section className={style['section']}>
+          <h2 className={style['section-title']}>여행정보 (상단)</h2>
+          <Info />
+        </section>
+        <section className={style['section']}>
+          <div className={style['flex-wrapper']}>
+            <h2 className={style['section-title']}>여행옵션</h2>
+            <div className={style['add-option']}>
+              <AddIcon />
+              <span>여행옵션 추가하기</span>
+            </div>
+          </div>
+        </section>
       </Container>
     </Box>
   )
