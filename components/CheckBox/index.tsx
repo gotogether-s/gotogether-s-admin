146e4d9e1ca0e25034afba6fba813bc6f255c4fe
hyeonah <hyeonah.hello@gmail.com>
2022-10-07 18:35:39 +0900

    #4 feat: Create if statement to update ages value

--- components/CheckBox/index.tsx
+++ components/CheckBox/index.tsx
@@ -1,15 +1,19 @@
 import { FormGroup, FormControlLabel, Checkbox } from '@mui/material'
+import { useDispatch } from 'react-redux'
+import { update, ageUpdate } from '@store/addProductSlice'
 import { useState } from 'react'
 import style from './CheckBox.module.scss'
 
 const CheckBox = props => {
   const { label, name, valueLists } = props
 
+  const dispatch = useDispatch()
+
   const [valueListsCheckedStatus, setValueListsCheckedStatus] = useState(() =>
     valueLists.map(l => false)
   )
 
-  const updateAddProductStateFromCheckBox = (e, valueList, valueListindex) => {
+  const updateAddProductStateFromCheckBox = (e, value, valueListindex) => {
     const { name, checked } = e.target
     const targetChecked = checked
 
@@ -17,6 +21,13 @@ const CheckBox = props => {
       return valueListsCheckedStatus.map(
         (valueListCheckedStatus, isCheckedIndex) => {
           if (isCheckedIndex === valueListindex) {
+            if (name === 'ages') {
+              dispatch(ageUpdate({ name, value, targetChecked }))
+            } else {
+              targetChecked
+                ? dispatch(update({ name, value }))
+                : dispatch(update({ name, value: '' }))
+            }
             return targetChecked
           } else {
             return valueListCheckedStatus
