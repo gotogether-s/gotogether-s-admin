536f78b28e9cc12a4a26bd4f941e3dad63e09ffc
hyeonah <hyeonah.hello@gmail.com>
2022-10-07 18:38:17 +0900

    #4 feat: Create age checkbox in Category component

--- components/AddProduct/Category/index.tsx
+++ components/AddProduct/Category/index.tsx
@@ -1,7 +1,18 @@
+import CheckBox from '@components/CheckBox'
 import style from './Category.module.scss'
 
+const agesProps = {
+  label: '연령대 그룹',
+  name: 'ages',
+  valueLists: ['10대', '20대', '30대', '40대', '50대', '60대', '70대 이상'],
+}
+
 const Category = () => {
-  return <></>
+  return (
+    <>
+      <CheckBox {...agesProps} />
+    </>
+  )
 }
 
 export default Category
