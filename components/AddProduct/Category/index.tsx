f03f90abaa819154024b8e9226e0834d3d8ab54e
hyeonah <hyeonah.hello@gmail.com>
2022-10-08 16:55:30 +0900

    #4 feat: Add continent dropdown to Category

--- components/AddProduct/Category/index.tsx
+++ components/AddProduct/Category/index.tsx
@@ -1,4 +1,5 @@
 import CheckBox from '@components/CheckBox'
+import DropDown from '@components/DropDown'
 import style from './Category.module.scss'
 
 const agesProps = {
@@ -7,9 +8,31 @@ const agesProps = {
   values: ['10대', '20대', '30대', '40대', '50대', '60대', '70대 이상'],
 }
 
+const continentProps = {
+  label: '대륙',
+  name: 'continent',
+  values: [
+    '괌/사이판/하와이',
+    '동남아시아',
+    '동남아프리카',
+    '대만',
+    '북미',
+    '북아프리카/중동',
+    '인도/주변국',
+    '유럽',
+    '일본',
+    '중국',
+    '중남미',
+    '코카서스',
+    '호주/뉴질랜드',
+  ],
+  placeholder: '대륙을 선택해주세요',
+}
+
 const Category = () => {
   return (
     <>
+      <DropDown {...continentProps} />
       <CheckBox {...agesProps} />
     </>
   )
