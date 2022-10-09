5885b70df70a594450587784c74ae21f1e00fd87
hyeonah <hyeonah.hello@gmail.com>
2022-10-09 15:17:14 +0900

    #4 feat: Update AddProduct state from DropDown onChange

--- components/DropDown/index.tsx
+++ components/DropDown/index.tsx
@@ -1,15 +1,31 @@
-import { Select, MenuItem, OutlinedInput } from '@mui/material'
+import { Select, MenuItem } from '@mui/material'
+import { useDispatch } from 'react-redux'
+import { update } from '@store/addProductSlice'
+import { useState } from 'react'
 import style from './DropDown.module.scss'
 
 const DropDown = props => {
   const { label, name, values, placeholder } = props
+
+  const dispatch = useDispatch()
+
+  const [selected, setSelected] = useState('')
+
+  const updateAddProductStateFromDropDown = e => {
+    const { name, value } = e.target
+    setSelected(value)
+    dispatch(update({ name, value }))
+  }
 
   return (
     <div className={style['input-wrapper']}>
       <div className={style['label']}>{label}</div>
       <Select
         sx={{ width: '100%' }}
-        size='small'>
+        size='small'
+        name={name}
+        value={selected}
+        onChange={updateAddProductStateFromDropDown}>
         {values.map((value, index) => (
           <MenuItem
             key={index}
