3871abf9400f1889ca7714284da23d25ced4c63c
hyeonah <hyeonah.hello@gmail.com>
2023-02-05 21:34:00 -0500

    #40 chore: Move signin feature to homepage and Remove sideBar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,24 @@
-import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material/styles'
-import theme from '@styles/theme'
-import { Provider } from 'react-redux'
-import store from 'store'
-import Layout from '@components/Layout'
-import '@styles/reset.css'
-import '@styles/muiOverride.scss'
-import '@styles/globals.scss'
-import '@styles/editor/Editor.css'
 import '@styles/editor/ColorPicker.css'
+import '@styles/editor/Editor.css'
 import '@styles/editor/Image.css'
 import '@styles/editor/Link.css'
 import '@styles/editor/Table.css'
-import '@styles/editor/Video.css'
 import '@styles/editor/Toolbar.css'
+import '@styles/editor/Video.css'
+import '@styles/globals.scss'
+import '@styles/muiOverride.scss'
+import '@styles/reset.css'
+import theme from '@styles/theme'
+import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import store from 'store'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <Component {...pageProps} />
       </Provider>
     </ThemeProvider>
   )
