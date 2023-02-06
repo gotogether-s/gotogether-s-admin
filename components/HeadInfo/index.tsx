7fa408dbaa214cd8dcf7dc739adfc9505fd0b0d3
hyeonah <hyeonah.hello@gmail.com>
2023-02-06 13:48:38 -0500

    #43 feat: Add translation copy and Apply translation feature to HeadInfo

--- components/HeadInfo/index.tsx
+++ components/HeadInfo/index.tsx
@@ -1,9 +1,19 @@
+import en from '@public/locales/en/headInfo.json'
+import ko from '@public/locales/ko/headInfo.json'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 const HeadInfo = ({ title }: any) => {
+  const router = useRouter()
+
+  const { locale } = router
+  const translate = locale === 'en' ? en : ko
+
   return (
     <Head>
-      <title>고투게더 관리자 페이지: {title}</title>
+      <title>
+        {translate['고투게더 관리자']}: {title}
+      </title>
     </Head>
   )
 }
